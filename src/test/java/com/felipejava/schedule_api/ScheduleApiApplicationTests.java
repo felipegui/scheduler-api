9e7050255f30861f0ee6ed62b85a00f3e1d8de4b
Felipe Gui <fguiengenharia@gmail.com>
2026-02-16 12:34:27 -0300

    Enviando repositório pra nuvem

---
+++ src/test/java/com/felipejava/schedule_api/ScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipejava.schedule_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
