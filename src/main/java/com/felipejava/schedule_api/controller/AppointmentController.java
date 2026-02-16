09ec4cabbc369498b618f6eda7c1588eb6e01d14
Felipe Guimarães <fguiengenharia@gmail.com>
2026-02-16 21:20:46 +0000

    Implementação da classe AppointmentController

---
+++ src/main/java/com/felipejava/schedule_api/controller/AppointmentController.java
@@ -0,0 +1,43 @@
+package com.felipejava.schedule_api.controller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipejava.schedule_api.infrastructure.entity.Appointment;
+import com.felipejava.schedule_api.services.AppointmentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @PostMapping
+    public ResponseEntity<Appointment> saveAppointment(@RequestBody Appointment appointment) {
+        return ResponseEntity.ok(appointmentService.saveAppointment(appointment));
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> deleteAppointment(@RequestParam String customer, @RequestParam LocalDateTime appointmentDateTime) {
+        appointmentService.deleteAppointment(appointmentDateTime, customer);
+        return ResponseEntity.noContent().build();
+    }
+    @GetMapping
+    public ResponseEntity<Appointment> appointmentOfTheDay(@RequestParam LocalDate date) {
+        return ResponseEntity.ok().body(appointmentService.appointmentOfTheDay(date));
+    }
+    @PutMapping
+    public ResponseEntity<Appointment> updateAppointment(@RequestBody Appointment appointment, @RequestParam String customer, @RequestParam LocalDateTime appointmentDateTime) {
+        return ResponseEntity.ok().body(appointmentService.updateAppointment(appointment, customer, appointmentDateTime));
+    }
+}
